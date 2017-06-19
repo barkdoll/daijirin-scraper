06e0e5681736c34f5cc02be30dca8d937c506ff9
Jesse Barkdoll <jessebarkdoll@gmail.com>
2017-06-19 13:05:22 -0400

    add python script to repo

---
+++ daijirin-scraper.py
@@ -0,0 +1,98 @@
+# Should change this to scrape weblio.jp instead of kotobank. They have a SLIGHTLY easier structure to work with for extracting the dictionary data
+
+import bs4 as bs
+import urllib.request
+import urllib.parse
+import sys
+
+
+# pushes complete entry into final output list
+def pushEntry():
+    if len(entries) < 1:
+        entries.append(html)
+    else:
+        entries.append('\n\n<div>' + html + '</div>')
+
+term = sys.argv[1]
+
+# Creates an ASCII-friendly URL to query a webbrowser search
+converted_term = urllib.parse.quote(term, safe='')
+url = 'http://www.weblio.jp/content/' + converted_term
+
+# Opens the url and extracts the source html usings bs4
+sauce = urllib.request.urlopen(url)
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+
+daijirin = soup.find('a', href="http://www.weblio.jp/cat/dictionary/ssdjj")
+
+# used to locate the div after it for extracting definitions
+daiji_header = daijirin.find_parent('div', class_="pbarT")
+
+# Finds the div containing the Daijirin definitions
+entry = daiji_header.find_next_sibling('div', class_='kijiWrp')
+
+entry_head = entry.find('div', class_='NetDicHead')
+
+entry_body = entry.find('div', class_='NetDicBody')
+
+yomigana = entry_head.find('b').get_text()
+
+definition = []
+parsed = ''
+html = []
+entries = []
+# Takes multi-definition entries and generates a list for output
+multi_divs = entry.find_all('div', style="margin-left:1.2em;")
+
+
+# I need to make this part a function that takes the term as an argument and runs through the code with it!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Checks for multiple definitions and adds list tags for proper html structure
+if len(multi_divs) > 1:
+    stripped = []
+
+    for i in multi_divs:
+        text = i.get_text()
+        stripped.append(text)
+
+    # Removes extra whitespaces in the definition strings
+    for j in stripped:
+        definition.append(' '.join(j.split()))
+
+    html.append('【' + term + '】 ' + yomigana)
+
+    # Creates list out of definitions
+    html.append('<ol>')
+
+    for k in definition:
+        html.append('<li>' + k + '</li>')
+
+    html.append('</ol>')
+
+    # Converts html list to one whole string for pushing to the entries list
+    html = '\n'.join(html)
+    pushEntry()
+
+
+# Checks for single definition and parses it in the html
+else:
+    one_div = entry_body.select_one("div div div").get_text()
+
+    definition = ' '.join(one_div.split())
+    html = '【' + term + '】 ' + yomigana + '<br />\n' + definition
+
+
+
+    pushEntry()
+
+
+
+print('\nEntries: ')
+for x in entries:
+    print(x)
+
+
+# #cont > div:nth-child(6) > div > div.NetDicBody > div > div > div > div
+
