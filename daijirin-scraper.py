ec52e8443cfbd2d333f4456fda4e6c068f16cd06
Jesse Barkdoll <jessebarkdoll@gmail.com>
2017-06-23 14:14:33 -0400

    fixed issue with multi-definition selections

--- daijirin-scraper.py
+++ daijirin-scraper.py
@@ -32,6 +32,8 @@ daiji_header = daijirin.find_parent('div', class_="pbarT")
 # Finds the div containing the Daijirin definitions
 entry = daiji_header.find_next_sibling('div', class_='kijiWrp')
 
+# BIG BIG BIG TODO: change this to a find_all that can distinguish from multiple definitions and allow the user to select the one he wants to keep. Also find the correct yomigana based on the users selection.
+
 entry_head = entry.find('div', class_='NetDicHead')
 
 entry_body = entry.find('div', class_='NetDicBody')
@@ -42,16 +44,24 @@ definition = []
 parsed = ''
 html = []
 entries = []
+
 # Takes multi-definition entries and generates a list for output
-multi_divs = entry.find_all('div', style="margin-left:1.2em;")
+def_numbers = entry.find_all('div', style="float:left")
+
+multi_defs = []
+for n in def_numbers:
+    multi_defs.append(n.next_sibling)
+
+
+print(multi_defs)
 
 # TODO: Put this in function so that multiple definitions can be generated for entries list
 
 # Checks for multiple definitions and adds list tags for proper html structure
-if len(multi_divs) > 1:
+if len(multi_defs) > 1:
     stripped = []
 
-    for i in multi_divs:
+    for i in multi_defs:
         text = i.get_text()
         stripped.append(text)
 
