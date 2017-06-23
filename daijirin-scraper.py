90e3fee23d43153d92688fe79b37bb2e0f6a65ff
Jesse Barkdoll <jessebarkdoll@gmail.com>
2017-06-23 15:26:05 -0400

    add TODO goal comments

--- daijirin-scraper.py
+++ daijirin-scraper.py
@@ -24,13 +24,13 @@ soup = bs.BeautifulSoup(sauce, 'lxml')
 
 daijirin = soup.find('a', href="http://www.weblio.jp/cat/dictionary/ssdjj")
 
-# used to locate the div after it for extracting definitions
+# Locates the header div that indicates the following definition is a Daijirin definition
 daiji_header = daijirin.find_parent('div', class_="pbarT")
 
-# Finds the div containing the Daijirin definitions
+# Finds the following div containing the Daijirin definitions
 entry = daiji_header.find_next_sibling('div', class_='kijiWrp')
 
-# BIG BIG BIG TODO: change this to a find_all that can distinguish from multiple definitions and allow the user to select the one he wants to keep. Also find the correct yomigana based on the users selection.
+# TODO: change this to a find_all that can distinguish from multiple definitions and allow the user to select the one he wants to keep. Also find the correct yomigana based on the users selection.
 
 entry_head = entry.find('div', class_='NetDicHead')
 
@@ -49,9 +49,6 @@ def_numbers = entry.find_all('div', style="float:left")
 multi_defs = []
 for n in def_numbers:
     multi_defs.append(n.next_sibling)
-
-
-print(multi_defs)
 
 # TODO: Put this in function so that multiple definitions can be generated for entries list
 
@@ -89,13 +86,16 @@ else:
     definition = ' '.join(one_div.split())
     html = '【' + term + '】 ' + yomigana + '<br />\n' + definition
 
-
-
     pushEntry()
 
-
+# TODO: Create function for clearing all items from entries list
 
 print('\nEntries: ')
 for x in entries:
     print(x)
 
+
+# TODO: Add GUI for Anki and add command that automatically fills the user's specified deinition field.
+
+# TODO: 1. GUI should contain dropdown menu to select desired field.
+# TODO: 2. In the case of multiple entries, the ability to select one that the user would like to use.
