e9780f0de80e1e8a2dd3664da94b1f1c6106695d
Jesse Barkdoll <jessebarkdoll@gmail.com>
2018-04-30 02:02:55 -0400

    add copyright/license  info

--- anki_addon/daijirin_scraper.py
+++ anki_addon/daijirin_scraper.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+'''
+Addon: Daijirin Definition Scraper
+Copyright: (c) Jesse Barkdoll 2017-2018 <https://github.com/barkdoll>
+
+This addon was created to grab and parse definitions from the
+大辞林 (daijirin) dictionary hosted on <https://weblio.jp/>
+
+
+License: GNU AGPLv3 or later <https://www.gnu.org/licenses/agpl.html>
+'''
+
 
 from aqt import mw, editor
 from aqt.utils import showInfo, tooltip, isWin
